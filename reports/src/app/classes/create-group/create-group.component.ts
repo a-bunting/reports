cd6a01e807315777d46c145c3d344fb374d37bf8
Bogomip <alex.bunting@gmail.com>
2021-06-22 19:11:44 +0300

    groups start

--- reports/src/app/classes/create-group/create-group.component.ts
+++ reports/src/app/classes/create-group/create-group.component.ts
@@ -12,4 +12,16 @@ export class CreateGroupComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  generateUserData() {
+    // get data and split into individual elements
+    let data: String[] = document.getElementById('groupInputBox').innerText.split("\n");
+    
+    // if there is a header row build a list of the keys to use for this dataset
+    const keys: string[] = data[0].split(",");
+    // iterate over the data and build a new array
+    data.forEach((row: string) => {
+        let newUser = row.split(",");
+    })
+}
+
 }
