0b1bb92d3b7d4c0593a5f2fdcc4ad6cdd60a65db
Bogomip <alex.bunting@gmail.com>
2021-07-11 22:06:00 +0300

    upd

--- reports/src/app/classes/edit-group/edit-group.component.ts
+++ reports/src/app/classes/edit-group/edit-group.component.ts
@@ -21,8 +21,19 @@ export class EditGroupComponent implements OnInit {
     ngOnInit(): void {
         this.isLoading = true;
         this.getAllGroups().subscribe((groups: QuerySnapshot<any>) => {
+            let keys: string[] = [];
+
             groups.forEach((grp: DocumentSnapshot<Group>) => {
-                this.groups.push(grp.data());
+                let newData = grp.data();
+                newData['id'] = grp.id;
+
+                // iterate through each of the keys...
+                Object.keys(grp).forEach((key: string) => {
+                    // fill the keys array without duplicates...
+
+                })
+
+                this.groups.push(newData);
             })
         }, (error) => {
             console.log(`Error loading groups: ${error.message}`);
@@ -33,6 +44,101 @@ export class EditGroupComponent implements OnInit {
 
     getAllGroups(): Observable<any> {
         return this.db.getGroups().pipe(take(1));
+    }
+
+    /**
+     * Chnage a value for one of the users.
+     * @param index 
+     * @param key 
+     * @param input 
+     */
+     userValueChange(index: number, key: string, input: FocusEvent | KeyboardEvent) {
+        const reference: HTMLElement = <HTMLElement>input.target;
+        const newValue = reference.innerText.split("\n");
+        this.groups[index][key] = newValue[0];
+    }
+
+    /**
+     * Remove a user
+     * @param index 
+     */
+    removeUser(index: number): void {
+        this.groups.splice(index, 1);
+    }
+
+    /**
+     * Chnage one of the column values - the titles.
+     * This needs to change each of the values in each of the user entries too.
+     * @param index 
+     * @param input 
+     */
+    columnValueChange(index: number, input: FocusEvent | KeyboardEvent) {
+        const reference: HTMLElement = <HTMLElement>input.target;
+        const newKeyArray = [...this.keys];
+        const newValue = reference.innerText.split("\n");
+
+        newKeyArray[index] = newValue[0];
+        
+        let newData = [];
+        
+        // iterate over the data and build a new array
+        this.groups.forEach((row) => {
+            let newDataUser = {};
+            // go through the array line by line changing the key values...
+            this.keys.forEach((key: string, i: number) => {
+                const newKey = { [newKeyArray[`${i}`]] : row[`${key}`] }
+                newDataUser = {...newDataUser, ...newKey};
+            })
+
+            // add ot the new array
+            newData.push(newDataUser);
+        })
+
+        this.keys = newKeyArray;
+        this.groups = newData;
+    }
+
+    /**
+     * Adds a new column to the data - will name it col length...
+     */
+    addColumn(): void {
+        // add to the keys...
+        const colName = `Column ${this.keys.length + 1}`;
+        this.keys.push(colName);
+
+        // new data
+        let newData = [...this.groups];
+
+        // iterate over and add to each of the userdata...
+        newData.forEach((row) => {
+            row[colName] = "";
+        });
+
+        this.groups = newData;
+    }
+
+    /**
+     * removes a column
+     * @param index 
+     */
+    deleteColumn(index: number): void {
+        // strip from the keys array
+        const colName: string = this.keys[index];
+        
+        // and remove from each of the user data array
+        this.groups.forEach(row => {
+            delete row[colName];
+        });
+
+        this.keys.splice(index, 1);
+    }
+
+    /**
+     * keyvalue pipe orders alphabetically automatically. This stop that.
+     * @returns number 0 
+     */
+    returnZero(): number {
+        return 0;
     }
 
 }
