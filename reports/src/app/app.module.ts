4a341968b366e05dbb014b1569973702804724bb
Bogomip <alex.bunting@gmail.com>
2021-10-24 21:10:20 +0300

    upd

--- reports/src/app/app.module.ts
+++ reports/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { EditReportComponent } from './reports/edit-report/edit-report.component
 import { JoinComponent } from './utilities/join/join.component';
 import { BugreportComponent } from './utilities/bugreport/bugreport.component';
 import { AdminBugReportsComponent } from './admin/admin-bug-reports/admin-bug-reports.component';
+import { NgxPayPalModule } from 'ngx-paypal';
 import { IntroComponent } from './utilities/intro/intro.component';
 
 @NgModule({
